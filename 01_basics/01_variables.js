e125a2b47706d4cba40ec302ea91034ee638a82e
Atul Singh <cocatul11@gmail.com>
2023-06-27 08:49:51 +0530

    committed basic variable naming paradigm in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const num = 100;
+let myName = "Atul Singh"
+var cityName = "Kolkata"
+districtName = "Howrah"
+
+console.table([num, myName, cityName, districtName])
--- 01_variables.js
+++
@@ -1,6 +0,0 @@
-const num = 100;
-let myName = "Atul Singh"
-var cityName = "Kolkata"
-districtName = "Howrah"
-
-console.table([num, myName, cityName, districtName])
